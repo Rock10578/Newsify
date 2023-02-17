6da73d87877edd136eddc3116f86ad7892dbfd7b
Shreyansh Patwa <shreyansh@Shreyanshs-MacBook-Air-2.local>
2023-02-18 00:11:31 +0530

    working on pagination

--- src/components/News.js
+++ src/components/News.js
@@ -2,26 +2,47 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    articles = []
     constructor(){
       super();
-      console.log("constructor running")
       this.state = {
-          articles: this.articles,
-          loading: false
+          articles: [],
+          loading: false,
+          page: 1,
       }
     }
 
     async componentDidMount(){
-      console.log('component')
-      let url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=1834ad42eb01433d83facfb182fd15c9";
+      let url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=1834ad42eb01433d83facfb182fd15c9&page=1&pageSize=30";
       let data = await fetch(url);
       let parsedData = await data.json()
       console.log(parsedData);
-      this.setState({articles: parsedData.articles})
+      this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
     }
+
+    handlePrev = async () => { 
+      let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=1834ad42eb01433d83facfb182fd15c9&page=${this.state.page - 1}&pageSize=30`;
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      this.setState({
+        page: this.state.page - 1,
+        articles: parsedData.articles
+      })
+    }
+    handleNext = async () => {
+      if (this.state.page + 1 > Math.ceil(this.state.totalResults/30)){
+
+      }else{
+        let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=1834ad42eb01433d83facfb182fd15c9&page=${this.state.page + 1}&pageSize=30`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({
+          page: this.state.page + 1,
+          articles: parsedData.articles
+        })
+      }
+    }
+
   render() {
-    console.log("render")
     return (
       <div className='container my-3'>
         <h2>Newsify - Top Headlines</h2>
@@ -31,6 +52,17 @@ export class News extends Component {
                         <NewsItem title={e.title?e.title.slice(0, 45):""} description={e.description?e.description.slice(0, 78):""} imageUrl={e.urlToImage} newsUrl={e.url}/>
                     </div>
             })}
+        </div>
+        <div className="my-3 ">
+          <nav aria-label="...">
+            <ul className="pagination d-flex justify-content-center">
+              <li className="page-item disabled p-3"><button disabled={this.state.page<=1} type="button" className="btn btn-primary" onClick={this.handlePrev}>Previous</button></li>
+              <li className={`page-item ${this.state.page === 1? 'active' : 'disabled'} p-3`}><button className="page-link">1<span className="visually-hidden">(current)</span></button></li>
+              <li className={`page-item ${this.state.page === 2? 'active' : 'disabled'} p-3`} aria-current="page"><button className="page-link">2</button></li>
+              <li className={`page-item ${this.state.page === 3? 'active' : 'disabled'} p-3`}><button className="page-link">3</button></li>
+              <li className="page-item p-3"><button type="button" className="btn btn-primary" onClick={this.handleNext}>Next</button></li>
+            </ul>
+          </nav>
         </div>
       </div>
     )
